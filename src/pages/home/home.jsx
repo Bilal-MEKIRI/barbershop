e242e7e049b8acc5d05fab5fdc7dbf0a30ad01d5
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-10-24 20:54:18 +0200

    Where To Find Us Section

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -57,6 +57,19 @@ export default function Home() {
         </div>
       </section>
 
+      <section className="location" id="location-section">
+        <div className="location-content-container">
+          <h1 className="section-title">Where To Find Us</h1>
+          <div className="address-container">
+            <img src='assets/icons/location_icon_50.png' alt="location icon" className="location-icon"></img>
+            <p className="address">123 Barber Lane Stylsville, HB 78901</p>
+          </div>
+          <div className="location-cards-container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d158857.83988761643!2d-0.2664059157476487!3d51.52873980477459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sfr!4v1698172211093!5m2!1sen!2sfr" className="map" style={{border: 0 }} allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title="googlemap" />
+          </div>
+        </div>
+      </section>
+
     </>
   );
 }
