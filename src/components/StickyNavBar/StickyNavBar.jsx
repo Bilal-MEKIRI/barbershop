729b01a86f44b19542c2419a8aaab1d350cefbbb
Bilal-MEKIRI <bilal.mekiri@gmail.com>
2023-10-29 15:26:31 +0100

    sticky nav bar max width 1800px

--- src/components/StickyNavBar/StickyNavBar.jsx
+++ src/components/StickyNavBar/StickyNavBar.jsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import MobileMenu from '../MobileMenu/MobileMenu';
 import './StickyNavBar.scss';
